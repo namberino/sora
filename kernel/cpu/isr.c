bf5ecc1379eecff96e50cda6607f97d5a25a6200
namber0 <namnb2402@gmail.com>
2024-05-08 12:03:28 +0700

    Add IRQ handling and PIC

--- kernel/cpu/isr.c
+++ kernel/cpu/isr.c
@@ -2,10 +2,14 @@
 #include "idt.h"
 #include "../drivers/screen.h"
 #include "../kernel/util.h"
+#include "../drivers/ports.h"
+
+isr_t int_handlers[256];
 
 // can't do this in a loop cuz we need the address of the function names
 void isr_install()
 {
+    // default processor interrupts
     set_idt_gate(0, (u32)isr0);
     set_idt_gate(1, (u32)isr1);
     set_idt_gate(2, (u32)isr2);
@@ -38,6 +42,36 @@ void isr_install()
     set_idt_gate(29, (u32)isr29);
     set_idt_gate(30, (u32)isr30);
     set_idt_gate(31, (u32)isr31);
+
+    // remap the PIC (reference https://wiki.osdev.org/PIC)
+    port_byte_out(0x20, 0x11);
+    port_byte_out(0xA0, 0x11);
+    port_byte_out(0x21, 0x20);
+    port_byte_out(0xA1, 0x28);
+    port_byte_out(0x21, 0x04);
+    port_byte_out(0xA1, 0x02);
+    port_byte_out(0x21, 0x01);
+    port_byte_out(0xA1, 0x01);
+    port_byte_out(0x21, 0x0);
+    port_byte_out(0xA1, 0x0); 
+
+    // install the irq
+    set_idt_gate(32, (u32)irq0);
+    set_idt_gate(33, (u32)irq1);
+    set_idt_gate(34, (u32)irq2);
+    set_idt_gate(35, (u32)irq3);
+    set_idt_gate(36, (u32)irq4);
+    set_idt_gate(37, (u32)irq5);
+    set_idt_gate(38, (u32)irq6);
+    set_idt_gate(39, (u32)irq7);
+    set_idt_gate(40, (u32)irq8);
+    set_idt_gate(41, (u32)irq9);
+    set_idt_gate(42, (u32)irq10);
+    set_idt_gate(43, (u32)irq11);
+    set_idt_gate(44, (u32)irq12);
+    set_idt_gate(45, (u32)irq13);
+    set_idt_gate(46, (u32)irq14);
+    set_idt_gate(47, (u32)irq15);
 
     set_idt(); // set idt using lidt
 }
@@ -87,4 +121,26 @@ void isr_handler(registers_t reg)
     kprint("\n");
     kprint(exception_msg[reg.int_no]);
     kprint("\n");
+}
+
+void register_int_handler(u8 n, isr_t handler)
+{
+    int_handlers[n] = handler;
+}
+
+// gets called from asm interrupt handler stub
+void irq_handler(registers_t reg)
+{
+    // send an EOI (end of interrupt) signal to the PICs after every interrupts (if this interrupt involved the slave)
+    if (reg.int_no >= 40)
+        port_byte_out(0xA0, 0x20); // send reset signal to slave
+    
+    port_byte_out(0x20, 0x20); // send reset signal to master
+
+    // interrupt handling
+    if (int_handlers[reg.int_no] != 0)
+    {
+        isr_t handler = int_handlers[reg.int_no];
+        handler(reg);
+    }
 }
