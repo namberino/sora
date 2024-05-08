4fc2b0b30c4bea5fa9c0a17eb51b496a5d3d1a0f
namber0 <namnb2402@gmail.com>
2024-05-08 13:19:15 +0700

    Revert "Add IRQ handling and PIC"
    This reverts commit bf5ecc1379eecff96e50cda6607f97d5a25a6200.

--- kernel/cpu/int.asm
+++ kernel/cpu/int.asm
@@ -1,5 +1,4 @@
 [extern isr_handler] ; found in isr.c
-[extern irq_handler] ; found in isr.c
 
 isr_common_stub: ; saves the processor state, sets up for kernel mode segments, calls the C-level fault handler, and finally restores the stack frame
     pusha ; pushes edi, esi, ebp, esp, ebx, edx, ecx, eax
@@ -24,29 +23,6 @@ isr_common_stub: ; saves the processor state, sets up for kernel mode segments, 
     add esp, 8 ; cleans up the pushed error code and pushed isr number
     sti
     iret ; pop 5 things at once: cs, eip, eflags, ss, and esp
-
-; similar to the isr code except for the irq_handler call and the pop ebx
-irq_common_stub:
-    pusha
-    mov ax, ds
-    push eax
-    mov ax, 0x10 ; kernel data segment descriptor
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-
-    call irq_handler
-
-    pop ebx ; reload original data segment descriptor
-    mov ds, bx
-    mov es, bx
-    mov fs, bx
-    mov gs, bx
-    popa
-    add esp, 8
-    sti
-    iret 
 
 ; we don't know which interrupt was caller when the handler is run, so we will need to have a different handler for every interrupt
 ; some interrupts push an error code onto the stack but others don't
@@ -303,100 +279,3 @@ isr31:
     push byte 0
     push byte 31
     jmp isr_common_stub
-
-; irq handlers
-irq0:
-	cli
-	push byte 0
-	push byte 32
-	jmp irq_common_stub
-
-irq1:
-	cli
-	push byte 1
-	push byte 33
-	jmp irq_common_stub
-
-irq2:
-	cli
-	push byte 2
-	push byte 34
-	jmp irq_common_stub
-
-irq3:
-	cli
-	push byte 3
-	push byte 35
-	jmp irq_common_stub
-
-irq4:
-	cli
-	push byte 4
-	push byte 36
-	jmp irq_common_stub
-
-irq5:
-	cli
-	push byte 5
-	push byte 37
-	jmp irq_common_stub
-
-irq6:
-	cli
-	push byte 6
-	push byte 38
-	jmp irq_common_stub
-
-irq7:
-	cli
-	push byte 7
-	push byte 39
-	jmp irq_common_stub
-
-irq8:
-	cli
-	push byte 8
-	push byte 40
-	jmp irq_common_stub
-
-irq9:
-	cli
-	push byte 9
-	push byte 41
-	jmp irq_common_stub
-
-irq10:
-	cli
-	push byte 10
-	push byte 42
-	jmp irq_common_stub
-
-irq11:
-	cli
-	push byte 11
-	push byte 43
-	jmp irq_common_stub
-
-irq12:
-	cli
-	push byte 12
-	push byte 44
-	jmp irq_common_stub
-
-irq13:
-	cli
-	push byte 13
-	push byte 45
-	jmp irq_common_stub
-
-irq14:
-	cli
-	push byte 14
-	push byte 46
-	jmp irq_common_stub
-
-irq15:
-	cli
-	push byte 15
-	push byte 47
-	jmp irq_common_stub
